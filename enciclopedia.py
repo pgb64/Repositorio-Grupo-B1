2c4f52912473f08ec62e6925149273ede56c6810
Guillermo Espinosa Ruiz <ger4@alu.ua.es>
2025-02-14 14:43:01 +0100

    Update enciclopedia.py

--- enciclopedia.py
+++ enciclopedia.py
@@ -1,6 +1,7 @@
 def obtener_definicion(tema):
     definicones = {
-        'Microsoft': 'Entidad vampírica que intenta monopolizar el mercado de la informática'
+        'Microsoft': 'Entidad vampírica que intenta monopolizar el mercado de la informática',
+        'Windows': 'Sistema operativo en tipo de archivo NFTS incompatible con Linux'
     }
     return definicones.get(tema.lower(), "Definición no encontrada")
 
